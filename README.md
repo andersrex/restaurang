471de18f2e3b51aad55eecb05e20bbfdc6b2d1a2
andersrex <rex.anders@gmail.com>
2015-01-12 18:53:05 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,105 @@
-# restaurang
+# Restaurang.js
+
+Restaurang.js makes it easy to consume RESTful APIs in Javascript.
+
+This was created because I wanted something similar to the excellent [Restangular](https://github.com/mgonto/restangular) outside AngularJS.
+
+Only supports JSON objects for now.
+
+## Disclaimer
+
+This is still very much work in progress. Come back in a few days for something more usable.
+
+## Install
+
+`<script src="restaurang.js"></script>`
+
+Include restaurang.js in your project.
+
+Restaurang.js has no external dependencies but uses ECMAScript 6 promises and some ECMAScript 5 features. This means IE users will need the [es6-promise](https://github.com/jakearchibald/es6-promise) polyfill and <IE9 users will need [es5-shim](https://github.com/es-shims/es5-shim).
+
+## Usage
+
+````javascript
+
+// Let's start by setting the base url
+Restaurang.setUrl("/api/v1");
+
+// Fetch one post
+Restaurang.one("posts", "1").get().then(function(post) { // GET /api/v1/posts/1
+  post.name = "Lorem ipsum...";
+
+  post.save(); // PUT /api/v1/articles/1
+
+  // Let's fetch the comments for this post
+  post.all("comments").getList().then(function(comments) { // GET /api/v1/posts/1/comments, expects an array
+
+    console.log(comments[0].id); // 1
+
+    comments[0].body = "Great story!";
+
+    comments[0].save(); // PUT /api/v1/posts/1/comments/1
+  });
+});
+
+// Fetch a comment without first fetching the posts
+Restaurang.one("posts", "1").one("comments", "1").get(); // GET /api/v1/posts/1/comments/1
+
+// Create a new article
+Restaurang.all("posts").post({ // POST /api/v1/posts
+  name: "Foo",
+  body: "Bar"
+}).then(function(post) {
+  console.log(post.body); // Bar
+});
+````
+
+## Methods
+
+Each Restaurang object have the following object methods (including `Restaurang` itself). Restaurang elements and collections also have some specific methods.
+
+### Object methods
+
+* one(route, id)
+* all(route)
+* setUrl(url)
+
+### Element methods
+
+* get([queryParams, headers])
+* getList(element, [queryParams, headers])
+* put([queryParams, headers])
+* post(element, data, [queryParams, headers])
+* remove([queryParams, headers])
+
+### Collection methods
+
+* get(id)
+* getList([queryParams, headers])
+* post(data, [queryParams, headers])
+
+### Restaurang.ajax
+
+If you need a custom AJAX function for checking/renewing tokens or just want to be fancy, you can replace `Restaurang.ajax` with a `jQuery.ajax` compatible method.
+
+## TODO
+
+* Add support for extending elements, nested elements and collections with methods
+* Add to npm and bower
+* Support for default options (headers etc.)
+* Tests
+* HEAD, TRACE, jsonp
+* ...
+* Profit!
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Anders Rex
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/restaurang.js
@@ -0,0 +1,241 @@
+/**
+ * Restaurang
+ * @version v0.0.1 - 2015-01-12
+ * @link https://github.com/andersrex/restaurang
+ * @author Anders Rex <anders.rex@iki.fi>
+ * @license MIT License, http://www.opensource.org/licenses/MIT
+ */
+
+'use strict';
+
+
+// Object methods
+
+// Creates a Restaurang object for one REST element
+function one(parent, route, id) {
+    if (!id) { throw new Error('Expected argument id for function.'); }
+
+    var url = parent._url + '/' + route + '/' + id;
+
+    return createRestaurangObject(url);
+}
+
+// Creates a Restaurang object for a REST collection
+function all(parent, route) {
+    var url = parent._url + '/' + route;
+
+    return createRestaurangObject(url, true);
+}
+
+function setUrl(current, url) {
+    current._url = url;
+    return current;
+}
+
+// TODO: Add function several(parent, route, ...) { ... }?
+
+
+// Element methods
+
+function elementGet(queryParams, headers) {
+    var options = createOptions(this._url, queryParams, headers);
+
+    return requestWithMethod('GET')(options);
+}
+
+function elementGetList(element, queryParams, headers) {
+    var url = this._url + '/' + element;
+    var options = createOptions(url, queryParams, headers);
+    options.isCollection = true;
+
+    return requestWithMethod('GET')(options);
+}
+
+function elementPut(queryParams, headers) {
+    var options = createOptions(this._url, queryParams, headers);
+    options.data = this;
+
+    return requestWithMethod('PUT')(options);
+}
+
+function elementPost(element, data, queryParams, headers) {
+    var url = this._url + '/' + element;
+    var options = createOptions(url, queryParams, headers);
+    options.data = data;
+
+    return requestWithMethod('POST')(options);
+}
+
+
+function elementRemove(queryParams, headers) {
+    var options = createOptions(this._url, queryParams, headers);
+
+    return requestWithMethod('DELETE')(options);
+}
+
+
+// Collection methods
+
+function collectionGet(id) {
+    if(!id) {
+        throw new Error('Expected argument id for function.');
+    }
+    return requestWithMethod('GET')({url: this._url + '/' + id});
+}
+
+function collectionGetList(queryParams, headers) {
+    var options = createOptions(this._url, queryParams, headers);
+    options.isCollection = true;
+
+    return requestWithMethod('GET')(options);
+}
+
+function collectionPost(data, queryParams, headers) {
+    var options = createOptions(this._url, queryParams, headers);
+    options.data = data;
+
+    return requestWithMethod('POST')(options);
+}
+
+
+// Attaching functions
+
+function attachElementMethods(RestaurangObject) {
+    RestaurangObject.get = elementGet;
+    RestaurangObject.getList = elementGetList;
+    RestaurangObject.put = elementPut;
+    RestaurangObject.post = elementPost;
+    RestaurangObject.remove = elementRemove;
+
+    return RestaurangObject;
+}
+
+function attachCollectionMethods(RestaurangObject) {
+    RestaurangObject.get = collectionGet;
+    RestaurangObject.getList = collectionGetList;
+    RestaurangObject.post = collectionPost;
+
+    return RestaurangObject;
+}
+
+function attachObjectMethods(RestaurangObject, url, isCollection) {
+    url = url || '';
+
+    RestaurangObject._url = url;
+    RestaurangObject._isCollection = isCollection;
+    RestaurangObject.one = function(route, id) { return one(this, route, id); };
+    RestaurangObject.all = function(route) { return all(this, route); };
+    RestaurangObject.setUrl = function(url) { return setUrl(this, url); };
+
+    return RestaurangObject;
+}
+
+function attachMethods(RestaurangObject, url, isCollection) {
+    RestaurangObject = attachObjectMethods(RestaurangObject, url, isCollection);
+
+    if (isCollection) {
+        RestaurangObject = attachCollectionMethods(RestaurangObject);
+    } else {
+        RestaurangObject = attachElementMethods(RestaurangObject);
+    }
+
+    return RestaurangObject;
+}
+
+// Performs an AJAX request
+function ajax(options) {
+    var request = new XMLHttpRequest();
+
+    options = options || {};
+    options.url = options.url || '/';
+    options.method = options.method || 'GET';
+    options.headers = options.headers || {};
+
+    request.open(options.method, options.url, true);
+
+    // Set request headers
+    for (var key in options.headers ) {
+        request.setRequestHeader(key, options.headers[key]);
+    }
+
+    // TODO: ?
+    request.setRequestHeader('Content-Type', 'application/json');
+
+    // console.log(options.method + ': ' + options.url)
+
+    request.send(JSON.stringify(options.data));
+
+    // Return promise
+    return new Promise(function(resolve, reject) {
+        request.onreadystatechange = function () {
+
+            if (request.readyState !== 4) { return; }
+
+            if (request.status === 200) {
+                // TODO: Add error handling
+                var data = JSON.parse(request.responseText);
+
+                resolve(data, request.statusText, request);
+            } else {
+                reject(request, request.status, request.statusText);
+            }
+        };
+    });
+}
+
+// Performs an AJAX request and attached Restaurang methods
+function request(options) {
+    return new Promise(function(resolve, reject) {
+        Restaurang.ajax(options).then(function(data) {
+
+            if(options.isCollection && !Array.isArray(data)) {
+                throw new Error('Expecting a collection as response.');
+            }
+
+            var restaurangObject = attachMethods(data, options.url, options.isCollection);
+
+            resolve(restaurangObject);
+        }, reject);
+    });
+}
+
+// Closure for creating the ajax methods
+function requestWithMethod(method) {
+    return function(options) {
+        options = options || {};
+        options.method = method;
+        return request(options);
+    };
+}
+
+// Convert a object to a uri string
+function objectToUri(obj) {
+    return Object.keys(obj).map(function(key) {
+        return key + '=' + obj[key];
+    }).join('&');
+}
+
+// Generates options object with url and header
+function createOptions(url, queryParams, headers) {
+    return {
+        url: url + (queryParams ? '?' +  objectToUri(queryParams) : ''),
+        headers: headers
+    };
+}
+
+// TODO: Add head, trace, options, jsonp...
+
+// Creates a Restaurang object
+function createRestaurangObject(url, isCollection) {
+    var RestaurangObject = {};
+
+    RestaurangObject = attachMethods(RestaurangObject, url, isCollection);
+
+    return RestaurangObject;
+}
+
+var Restaurang = createRestaurangObject('');
+
+Restaurang.ajax = ajax;
+
+module.exports = Restaurang;
